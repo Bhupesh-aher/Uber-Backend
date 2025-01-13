4d1bf12ab877b6d227b2ef245db8caa2e4ba9005
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-01-13 14:47:25 +0530

    created user schema

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minLength: [3, "First Name must be at least 3 characters long"],
+        },
+        lastname: {
+            type: String,
+            minLength: [3, "last Name must be at least 3 characters long"],
+        },
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: [4, "email must be at least 4 characters long"]
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    socketId: {
+        type: String,
+    }
+})
+
+
+
+
+
+userSchema.methods.genrateAuthToken = async function () {
+    const user = this;
+    const token = await jwt.sign({_id: user._id}, process.env.JWT_SECRET)
+    return token;
+}
+
+userSchema.methods.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10)
+}
+
+userSchema.methods.comaprePassword = async function (passwordInputByUser) {
+    const user = this;
+    const passwordHash = user.password;
+    return await bcrypt.compare(passwordInputByUser, passwordHash)
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const http = require("http")
 const app = require("./app")
 const port = process.env.PORT || 3000;
+const connectDB = require("./db/database");
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-    console.log(`server is successfully listening on PORT ${port}...`);
+
+connectDB().then(() => {
+    console.log("database connected successfully");
+    server.listen(port, () => {
+        console.log(`server is successfully listening on PORT ${port}...`);
+        
+    })
+    
+})
+.catch(() => {
+    console.log("Database cannot be connected");
     
 })
