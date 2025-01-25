e8668751397a7f2dce788c23254b88aa0eef221c
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-01-25 19:45:58 +0530

    created get distance & time API

--- routes/maps.js
+++ routes/maps.js
@@ -6,8 +6,17 @@ const {query} = require('express-validator');
 
 mapsRouter.get("/get-coordinates", 
     query('address').isString().isLength({min: 3}),
-    auth.userAuth, mapController.getCoordinates
+    auth.userAuth, 
+    mapController.getCoordinates
 )
 
+mapsRouter.get("/get-distance-time",
+    query("origin").isString().isLength({min: 3}),
+    query('destination').isString().isLength({min: 3}),
+    auth.userAuth,
+    mapController.getDistanceTime
+
+
+ )
 
 module.exports = mapsRouter;
--- services/mapsService.js
+++ services/mapsService.js
@@ -22,3 +22,32 @@ module.exports.getAddressCordinate = async (address) => {
         throw error;
     }
 }
+
+
+module.exports.getDistanceTime = async(origin, destination) => {
+    if(!origin || !destination){
+        throw new Error("Origin and destimation are required")   
+    }
+    const apikey = process.env.GOOGLE_MAPS_API;
+ 
+    const url = `https://maps.gomaps.pro/maps/api/distancematrix/json?destinations=${encodeURIComponent(destination)}&origins=${encodeURIComponent(origin)}&key=${apikey}`
+
+    try{
+        const response = await axios.get(url);
+        if(response.data.status === "OK"){
+            if(response.data.rows[0].elements[0].status === "ZERO_RESULTS"){
+                throw new Error("No routes found")
+            }
+
+            return response.data.rows[0].elements[0]
+        }
+        else{
+            throw new Error('Unable to fetch distance and time')
+        }
+    }
+    catch(error){
+        console.log(err);
+        throw err;
+        
+    }
+}
